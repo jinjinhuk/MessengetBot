0728acba792df3f26b11a98eac82a49ed4202f96
MainFe <jungga2019@gmail.com>
2025-01-10 06:40:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,26 +2,27 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    const msg = req.query.msg;
-    console.log(JSON.stringify(msg));
-    if (msg) {
-      if(msg == '1') {
-        res.send('1번 매크로');
-      }
-      else if(msg == '2') {
-        res.send('2번 맼');
-      }
-      else if(msg == '3방') {
-        res.send('어ㅓㅓ 3'); 
-      }
-      else {
-        res.send(msg + 'v2');
-      }
-    }
-    else {
-      res.send('서버 작동 확인'); // 클라이언트에게 "서버 작동 확인" 응답
-    }
+app.get('/:msg', (req, res) => {
+    const msg = req.params.msg;
+    console.log(msg);
+    res.send('받은 메시지: ${msg}')
+    // if (msg) {
+    //   if(msg == '1') {
+    //     res.send('1번 매크로');
+    //   }
+    //   else if(msg == '2') {
+    //     res.send('2번 맼');
+    //   }
+    //   else if(msg == '3방') {
+    //     res.send('어ㅓㅓ 3'); 
+    //   }
+    //   else {
+    //     res.send(msg + 'v2');
+    //   }
+    // }
+    // else {
+    //   res.send('서버 작동 확인'); // 클라이언트에게 "서버 작동 확인" 응답
+    // }
 });
 
 app.listen(PORT, () => {
